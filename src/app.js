79d38382c74317f0407c535e2f9d86cc2bf84fc8
Tanner Stephens <tannerrstephens@gmail.com>
2020-02-14 00:37:01 -0600

    Alignment

--- src/app.js
+++ src/app.js
@@ -1,72 +1,153 @@
 import * as PIXI from 'pixi.js';
-import * as Heap from 'heap';
+
+Number.prototype.mod = function(n) {
+    return ((this%n)+n)%n;
+};
+
+Set.prototype.union = function(setB) {
+    for (let elem of setB) {
+        this.add(elem);
+    }
+};
 
 const app = new PIXI.Application();
 
-const vel = 10;
-const numTris = 5000;
-const scale = 0.25;
+const vel = 3;
+const numTris = 100;
+const scale = 0.5;
+const gridSize = 100;
+const awareRadius = 200;
+const gridWidth = Math.ceil(window.innerWidth/gridSize);
+const gridHeight = Math.ceil(window.innerHeight/gridSize);
 
-window.onresize = () => {
-    app.renderer.view.style.position = "absolute";
-    app.renderer.view.style.display = "block";
-    app.renderer.autoResize = true;
-    app.renderer.resize(window.innerWidth, window.innerHeight);
-}
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
 
-window.onresize();
+const main = () => {
+    const createTriangle = () => {
+        const tri = new PIXI.Graphics();
 
-const createTriangle = () => {
-    const tri = new PIXI.Graphics();
+        tri.beginFill(0xffffff);
+        tri.lineStyle(0, 0xffffff);
+        tri.drawPolygon([0,0,10,30,20,0,10,5]);
+        tri.endFill();
 
-    tri.beginFill(0xffffff);
-    tri.lineStyle(0, 0xffffff);
-    tri.drawPolygon([0,0,10,30,20,0,10,5]);
-    tri.endFill();
+        return new PIXI.Sprite(app.renderer.generateTexture(tri));
+    }
 
-    return new PIXI.Sprite(app.renderer.generateTexture(tri));
-}
+    const getGridCoords = (tri) => {
+        return {
+            x: Math.floor(tri.x/gridSize),
+            y: Math.floor(tri.y/gridSize)
+        };
+    };
 
-const tris = Array(numTris).fill(null).map(() => createTriangle());
+    const getGrid = (coords) => {
+        return grid[coords.x][coords.y];
+    };
 
-tris.forEach((tri) => {
-    tri.x = Math.random() * window.innerWidth;
-    tri.y = Math.random() * window.innerHeight;
-    tri.anchor.set(0.5);
-    tri.rotation = Math.random() * 2;
-    tri.scale.set(scale);
-    app.stage.addChild(tri);
-});
+    const awareGrids = (tri) => {
+        const delta = [-awareRadius, 0, awareRadius];
 
-const dist = (t1, t2) => {
-    const dx = Math.min(Math.abs(t2.x - t1.x), Math.abs(window.innerWidth - t2.x - t1.x));
-    const dy = Math.min(Math.abs(t2.y - t1.y), Math.abs(window.innerHeight - t2.y - t1.y));
+        let result = new Set();
 
-    return Math.sqrt(dx**2 + dy**2);
-}
+        const seen = [];
 
-app.ticker.add(() => {
-    tris.forEach((tri) => {
-        // const nearest = Heap.nsmallest(tris, 3, (a, b) => {return dist(a, tri) < dist(b,tri)});
-        // const near = nearest.filter(tri2 => dist(tri2, tri) < 5 && tri2 != tri);
+        delta.forEach((dx) => {
+            delta.forEach((dy) => {
+                const coords = getGridCoords({
+                    x: Math.floor((tri.x + dx)).mod(window.innerWidth),
+                    y: Math.floor((tri.y + dy)).mod(window.innerHeight)
+                });
 
-        tri.x -= vel*Math.sin(tri.rotation);
-        tri.y += vel*Math.cos(tri.rotation);
+                if(!seen.includes(coords)) {
+                    seen.push(coords);
+                    result.union(getGrid(coords));
+                }
+            });
+        });
 
-        tri.rotation += (Math.random() - 0.5) * 0.1;
+        return Array.from(result);        
+    }
 
-        if (tri.x < 0) {
-            tri.x = window.innerWidth - Math.abs(tri.x)%window.innerWidth;
-        } else if (tri.x > window.innerWidth) {
-            tri.x = tri.x%window.innerWidth;
+    const grid = Array(gridWidth).fill(null).map(() => Array(gridHeight).fill(null).map(() => new Set()));
+
+    const tris = Array(numTris).fill(null).map(() => createTriangle());
+
+
+    tris.forEach((tri, i) => {
+        if (i == 0) {
+            tri.tint += 0xff0000
         }
+        tri.x = Math.random() * window.innerWidth;
+        tri.y = Math.random() * window.innerHeight;
+        tri.anchor.set(0.5);
+        tri.rotation = Math.random() * 2;
+        tri.scale.set(scale);
+        app.stage.addChild(tri);
 
-        if (tri.y < 0) {
-            tri.y = window.innerHeight - Math.abs(tri.y)%window.innerHeight;
-        } else if (tri.y > window.innerHeight) {
-            tri.y = tri.y%window.innerHeight;
-        }
+        const coords = getGridCoords(tri);
+
+        grid[coords.x][coords.y].add(tri);
     });
-});
 
-export default app;
+    const dist = (t1, t2) => {
+        const dx = Math.min(Math.abs(t2.x - t1.x), Math.abs(window.innerWidth - t2.x - t1.x));
+        const dy = Math.min(Math.abs(t2.y - t1.y), Math.abs(window.innerHeight - t2.y - t1.y));
+
+        return dx**2 + dy**2;
+    };
+
+    app.ticker.add(() => {
+        tris.forEach((tri, i) => {
+            const oldCoords = getGridCoords(tri);
+
+            const nearBoids = awareGrids(tri).filter((t2) => dist(tri, t2) < awareRadius);
+
+
+            const align = nearBoids.reduce((prev, cur) => {
+                return prev += cur.rotation 
+            }, 0) / nearBoids.length;
+
+            if(align > tri.rotation) {
+                tri.rotation += 0.03;
+            } else if (align < tri.rotation) {
+                tri.rotation -= 0.03;
+            }
+
+            const center = 0;
+
+            const newX = tri.x - vel*Math.sin(tri.rotation);
+            const newY = tri.y += vel*Math.cos(tri.rotation);
+
+            tri.x = newX;
+            tri.y = newY;
+
+            if (tri.x < 0) {
+                tri.x = window.innerWidth - Math.abs(tri.x).mod(window.innerWidth);
+            } else if (tri.x > window.innerWidth) {
+                tri.x = tri.x.mod(window.innerWidth);
+            }
+
+            if (tri.y < 0) {
+                tri.y = window.innerHeight - Math.abs(tri.y).mod(window.innerHeight);
+            } else if (tri.y > window.innerHeight) {
+                tri.y = tri.y.mod(window.innerHeight);
+            }
+
+            const coords = getGridCoords(tri);
+
+            if (oldCoords != coords) {
+                getGrid(oldCoords).delete(tri);
+                getGrid(coords).add(tri);
+            }
+        });
+    });
+};
+
+export {
+    app,
+    main
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import app from './app';
+import {app, main} from './app';
 
 document.addEventListener("DOMContentLoaded", () => {
     document.body.appendChild(app.view);
     document.body.style.margin = 0;
+    main();
 })
